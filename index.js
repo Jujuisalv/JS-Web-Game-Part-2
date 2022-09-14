c691e81e7582af9b4f2bc96fbef8ccbf926c44b8
Julius Alvarez <Juliusisaiaha@gmail.com>
2022-09-13 19:02:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ function newImage(url, left, bottom){
     return object
 }
 
-function newItem(url, left, bottom){
+function newItem(url, left, bottom) {
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function () {
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
 
-function newInventory(){
-    let inventory = document.createElement('div')
+function newInventory() {
+    let inventory= document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
@@ -27,6 +33,7 @@ function newInventory(){
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
 }
+
 
 newInventory()
 newImage('assets/green-character.gif', 100, 250)
